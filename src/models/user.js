e8713738586c3df7e2ca7979094f39b2f547dc5b
Siddharth Kundu <sk31395@gmail.com>
2018-11-18 13:13:07 +0530

    commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { hash } from 'bcryptjs'
 
 const userSchema = new mongoose.Schema(
   {
@@ -11,5 +12,16 @@ const userSchema = new mongoose.Schema(
     timestamps: true
   }
 )
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    try {
+      this.password = await hash(this.password, 10)
+    } catch (err) {
+      next(err)
+    }
+  }
+  next()
+})
 
 export default mongoose.model('User', userSchema)
--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,9 +1,30 @@
+import mongoose from 'mongoose'
+import { User } from '../models'
+import { UserInputError } from 'apollo-server-core'
+
 export default {
   Query: {
-    users: (root, arg, context, info) => {},
-    user: (root, arg, context, info) => {}
+    users: (root, arg, context, info) => {
+      // TODO: authenticated, projection, pagination, sanitization
+
+      return User.find({})
+    },
+    user: (root, { id }, context, info) => {
+      // TODO: auth, projection, sanitization
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new UserInputError(`${id} is not a valid user ID`)
+      }
+
+      return User.findById(id)
+    }
   },
   Mutation: {
-    signUp: (root, arg, context, info) => {}
+    signUp: (root, args, context, info) => {
+      // TODO: not auth
+
+      // validation
+
+      return User.create(args)
+    }
   }
 }
