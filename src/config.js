e25cccffbc4f9391c5338dd6bbbc6c09005fa37d
Siddharth Kundu <sk31395@gmail.com>
2019-01-04 00:03:37 +0530

    added session variables

--- src/config.js
+++ src/config.js
@@ -5,7 +5,15 @@ export const {
   DB_PASSWORD = 'test123',
   DB_HOST = 'ds055990.mlab.com',
   DB_PORT = '55990',
-  DB_NAME = 'local-chat'
+  DB_NAME = 'local-chat',
+
+  SESS_NAME = 'sid',
+  SESS_SECRET = 'ssh!secret',
+  SESS_LIFETIME = 1000 * 60 * 60 * 2,
+
+  REDIS_HOST = 'localhost',
+  REDIS_PORT = 6379,
+  REDIS_PASS = 'secret'
 } = process.env
 
 export const IN_PROD = NODE_ENV === 'production'
