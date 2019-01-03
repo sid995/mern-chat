d1c8c3589a2b29b4905214e190477e506510f71f
Siddharth Kundu <sk31395@gmail.com>
2019-01-04 00:03:59 +0530

    added express-sessions and connectRedis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { ApolloServer } from 'apollo-server-express'
 import mongoose from 'mongoose'
 import express from 'express'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
 import typeDefs from './typeDefs'
 import resolvers from './resolvers'
 import {
@@ -10,7 +12,13 @@ import {
   DB_PASSWORD,
   DB_HOST,
   DB_PORT,
-  DB_NAME
+  DB_NAME,
+  SESS_NAME,
+  SESS_SECRET,
+  SESS_LIFETIME,
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_PASS
 } from './config'
 ;(async () => {
   try {
@@ -24,10 +32,34 @@ import {
 
     app.disable('x-powered-by')
 
+    const RedisStore = connectRedis(session)
+
+    const store = new RedisStore({
+      host: REDIS_HOST,
+      port: REDIS_PORT,
+      pass: REDIS_PASS
+    })
+
+    APP_PORT.use(
+      session({
+        store,
+        name: SESS_NAME,
+        secret: SESS_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          maxAge: SESS_LIFETIME,
+          sameSite: true,
+          secure: IN_PROD
+        }
+      })
+    )
+
     const server = new ApolloServer({
       typeDefs,
       resolvers,
-      playground: !IN_PROD
+      playground: !IN_PROD,
+      context: ({ req, res }) => ({ req, res })
     })
 
     server.applyMiddleware({ app })
